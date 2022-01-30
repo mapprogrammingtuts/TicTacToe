1346e8d545aa418b4f41816e19db4338e6ee7efc
Bk <Bigbk75@gmail.com>
2022-01-30 16:20:31 +0200

    Implemented InitializeGame and MakeMove

--- Program.vb
+++ Program.vb
@@ -12,7 +12,9 @@ Module Program
     Sub Main()
         Console.WriteLine("---------------TIC TAC TOE----------------")
         InitializeGame()
+
         Do
+
             MakeMove()
             PrintBoard()
             CheckWinner()
@@ -20,5 +22,41 @@ Module Program
         Loop Until gameOver
         PrintGameOverMessage()
     End Sub
+    Sub MakeMove()
+        Console.WriteLine($"Player {currentPlayer}, Enter your move in the form 'column,row' e.g 2,3")
+        Dim move As String = Console.ReadLine() ' col,row
+        'Get the first character in the move, parse that as an integer
+        'Get the last character in the move, parse that as an integer
+        Dim col As Integer = Integer.Parse(move(0)) - 1
+        Dim row As Integer = Integer.Parse(move(2)) - 1
 
+        'Now we play at the position of the board
+
+        board(row * 3 + col) = currentPlayer
+
+        If currentPlayer = PlayerO Then
+            currentPlayer = PlayerX
+        Else
+            currentPlayer = PlayerO
+        End If
+
+
+    End Sub
+
+    Sub InitializeGame()
+        For i = 0 To 8
+            board(i) = Empty
+        Next
+        gameOver = False
+        winner = Empty
+        Dim rand As New Random()
+        Dim choice = rand.Next(2) ' 0 or 1
+        If choice = 0 Then
+            currentPlayer = PlayerO
+        Else
+            currentPlayer = PlayerX
+        End If
+
+
+    End Sub
 End Module
